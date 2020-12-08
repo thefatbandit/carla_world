0aaffe1371dbdb7589d0d6f085e5036aa82c7a86
thefatbandit <arnesh.issar@gmail.com>
2020-12-08 14:16:22 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='carla_world',
+      version='0.0.1',
+      install_requires=['numpy', 'pygame'] 
+)
---
+++ test.py
@@ -0,0 +1,35 @@
+import gym
+import gym_carla
+import carla
+
+from world import 
+
+def main():
+    # Parameters 
+    Params = {
+        'number_of_vehicles': 100,
+        'number_of_walkers': 0,
+        'display_size': 256,  # screen size of bird-eye render
+        'max_past_step': 1,  # the number of past steps to draw
+        'dt': 0.1,  # time interval between two frames
+        'discrete': False,  # whether to use discrete control space
+        'discrete_acc': [-3.0, 0.0, 3.0],  # discrete value of accelerations
+        'discrete_steer': [-0.2, 0.0, 0.2],  # discrete value of steering angles
+        'continuous_accel_range': [-3.0, 3.0],  # continuous acceleration range
+        'continuous_steer_range': [-0.3, 0.3],  # continuous steering angle range
+        'ego_vehicle_filter': 'vehicle.lincoln*',  # filter for defining ego vehicle
+        'port': 2000,  # connection port
+        'town': 'Town02',  # which town to simulate
+        'task_mode': 'random',  # mode of the task, [random, roundabout (only for Town03), debug (for fixed starting point)]
+        'max_time_episode': 1000,  # maximum timesteps per episode
+        'max_waypt': 12,  # maximum number of waypoints
+        'obs_range': 32,  # observation range (meter)
+        'lidar_bin': 0.125,  # bin size of lidar sensor (meter)
+        'd_behind': 12,  # distance behind the ego vehicle (meter)
+        'out_lane_thres': 2.0,  # threshold for out of lane
+        'desired_speed': 8,  # desired speed (m/s)
+        'max_ego_spawn_times': 200,  # maximum times to spawn ego vehicle
+        'display_route': True,  # whether to render the desired route
+        'pixor_size': 64,  # size of the pixor labels
+        'pixor': False
+    }
